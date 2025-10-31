32aeac419b755cb8785e9127c58cf10ae4bf5717
Silki <129131071+silkiy@users.noreply.github.com>
2025-10-31 11:28:18 +0700

    feat: Add cors dependency and its type definitions

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express, { Request, Response } from "express";
 import webRouter from "./routers/index";
 import { parse } from "url";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+app.use(
+    cors({
+        origin: ["http://localhost:3000", "https://gist-ai.vercel.app"],
+        methods: ["GET", "POST", "OPTIONS"],
+        allowedHeaders: ["Content-Type"],
+    })
+)
 
 app.use("/api", webRouter);
 
