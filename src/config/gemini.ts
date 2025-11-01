ae405f854794c7b879c1190647f6fc43818ad03e
Silki <129131071+silkiy@users.noreply.github.com>
2025-11-01 10:56:28 +0700

    fix: Update gemini model version to "gemini-2.0-flash"

--- src/config/gemini.ts
+++ src/config/gemini.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const geminiModel = new ChatGoogleGenerativeAI({
-    model: "gemini-2.5-pro",
+    model: "gemini-2.0-flash",
     temperature: 0,
     apiKey: process.env.GOOGLE_API_KEY!,
 });
